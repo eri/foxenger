3a836cd5472d6f10ce9d44fab9ac5e75ed593fa6
eri <contact.erionline@gmail.com>
2021-02-14 17:32:00 +0100

    Introduce the login and signin modals

--- app.py
+++ app.py
@@ -2,19 +2,20 @@
 from urllib.parse import unquote
 import datetime
 import json
+import time
 import os
 
 # Définir notre application Flask
 app = Flask(__name__)
 
 ### Fonctions de base
-
 def get_users():
     """Permet de retourner tous les utilisateurs depuis la BDD"""
     with open('db/users.json') as f: return json.load(f)
 def get_messages():
     """Permet de retourner tous les messages depuis la BDD"""
     with open('db/messages.json') as f: return json.load(f)
+
 def get_user(id_user = False, name_user = False):
     """Permet de retourner un seul utilisateur depuis la BDD"""
     users = get_users()['users']
@@ -22,27 +23,45 @@ def get_user(id_user = False, name_user = False):
         if id_user and int(x['id']) == id_user: return x
         if name_user and str(x['pseudo']) == name_user: return x
     return None
+def update_user(key, value, id_user = False, name_user = False):
+    """Permet de mettre à jour un utilisateur"""
+    data = get_users()
+    for x in data['users']:
+        if id_user and int(x['id']) == id_user or str(x['pseudo']) == name_user:
+            x[key] = value
+            with open('db/users.json', 'w') as f: json.dump(data, f)
+            return True
+    return False
+
 def get_message(id_msg):
     """Permet de retourner un seul message depuis la BDD"""
     msgs = get_messages()
     for x in msgs:
         if int(x['id']) == id_msg: return x
     return None
+
 def get_emojis():
     return ['😀','😂','😉','😅','😜','🤔','😡','😬','😷','👍','👎']
+def get_connected_users():
+    users = get_users()['users']
+    connected_users = []
+    for u in users:
+        if 'last_action' in u:
+            now = datetime.datetime.now().timestamp()
+            date_action = datetime.datetime.strptime(u['last_action'], '%Y-%m-%d %H:%M:%S.%f').timestamp()
+            # L'utilisateur n'est pas connecté depuis + de 60 secondes
+            if int(now-date_action) <= 60: connected_users.append(u)
+    return connected_users
 
-## Routes HTML
+## Routes HTML 
 @app.route("/")
 def page_messagerie():
+    """Page d'accueil du site"""
+    logged = False
+    if 'user_id' in session: logged = True
 
-    if 'user_id' in session:
-        # Déjà connecté, on montre les messages
-        return render_template("messenger.html", **{
-            'user':get_user(id_user=session.get('user_id')),
-            'nb_msg':len(get_messages()['messages']), 'emojis':get_emojis() })
-
-    # Utilisateur non enregistré
     return render_template("messenger.html", **{
+            'session': False,
             'user':get_users()['users'][-1], 'emojis':get_emojis(),
             'nb_msg': len(get_messages()['messages']) })
 
@@ -54,42 +73,62 @@ def favicon():
 
 ## Requêtes API
 @app.route("/api/user/new/<user>/<pw>")
-def add_users(user, pw):
+def add_user(user, pw):
     """Permet d'ajouter un utilisateur"""
-    data = get_users()
-    data['users'].append({"nom": str(user), "passe":str(pw)}) 
-    with open('db/users.json', 'w') as f:
-        json.dump(data, f)
+    try:
+        data = get_users()['users']
+        data.append({"id": data[-1]['id']+1, "nom": str(user), "passe":str(pw)}) 
+        with open('db/users.json', 'w') as f: json.dump(data, f)
+        return "True"
+    except Exception:
+        return "False"
+    
 
 @app.route("/api/message/new/<message>", methods=['GET', 'POST'])
 def add_messages(message):
     """Permet de retourner tous les messages depuis la BDD"""
     data = get_messages()
     user = get_users()['users'][-1]
-    data['messages'].append({"id": int(data['messages'][-1]['id'])+1, "id_membre":user['id'], "contenu":unquote(message), "date" : str(datetime.datetime.now())})
-    with open('db/messages.json', 'w') as f:
-        json.dump(data, f)
+    data['messages'].append({"id": int(data['messages'][-1]['id'])+1,
+    "id_membre":user['id'], "contenu":unquote(message), "date" : str(datetime.datetime.now())})
+    with open('db/messages.json', 'w') as f: json.dump(data, f)
     return data['messages'][-1]['contenu'] # Retourne le dernier message
 
 @app.route("/api/messages")
 def list_messages():
     """Permet de retourner tous les messages de la BDD"""
-    data = get_messages()['messages']
-    html = ""
-    for msg in data:
-        date = msg['date'].split(' ')[0]
-        heure = str(msg['date'].split(' ')[1].split('.')[0])[:5]
-        timestamp = date if not str(datetime.datetime.now()).split(' ')[0] == date else heure
-        user = get_users()['users'][-1]
-        html += f"<div class='authorbox'>{user['pseudo']}: <div class='msgbox'>{msg['contenu']}</div><small>{timestamp}</small></div>"
-    
-    if not 'lastMsg' in session or not session['lastMsg'] == get_messages()['messages'][-1]['id']:
-        # Session invalide ou nouveau message
-        session['lastMsg'] = get_messages()['messages'][-1]['id']
+    try:
+        data = get_messages()['messages']
+        html = ""
+        for msg in data:
+            date = msg['date'].split(' ')[0]
+            heure = str(msg['date'].split(' ')[1].split('.')[0])[:5]
+            timestamp = date if not str(datetime.datetime.now()).split(' ')[0] == date else heure
+            user = get_users()['users'][-1]
+            html += f"<div class='authorbox'>{user['pseudo']}: <div class='msgbox'>{msg['contenu']}</div><small>{timestamp}</small></div>"
+        if not 'lastMsg' in session or not session['lastMsg'] == get_messages()['messages'][-1]['id']:
+            # Session invalide ou nouveau message
+            session['lastMsg'] = get_messages()['messages'][-1]['id']
+        return html
+    except json.decoder.JSONDecodeError:
+        print("Les messages n'ont pas pu être vérifiés.") 
+        return "False"
 
-    print(session['lastMsg'])
-    return html
+@app.route("/api/user/status", methods=['POST'])
+def mark_user_as_online():
+    update_user("last_action", str(datetime.datetime.now()), name_user="paul")
+    return "False"
 
+@app.route("/api/users/online", methods=['GET'])
+def list_connected_users():
+    try:
+        users = get_connected_users()
+        if len(users) == 0: return "Aucun utilisateur n'est actuellement en ligne."
+        if len(users) == 1: return f"Uniquement {users[0]['pseudo']} est en ligne actuellement."
+        return f"Il y a {len(users)} utilisateurs actuellement en ligne : {', '.join([x['pseudo'] for x in users])}"
+    except json.decoder.JSONDecodeError:
+        print("Les utilisateurs en ligne n'ont pas pu être vérifiés.")
+        return "False"
 
 if __name__ == '__main__':
     app.secret_key = "X6DGXy9jGT6z8KjM3sAY"
--- static/js/messenger.js
+++ static/js/messenger.js
@@ -4,17 +4,93 @@ var msgbox = document.getElementById("message"); // Champ de message
 var writing = document.getElementById("typingMsg"); // Box "typing"
 var bdd = document.getElementById("bdd"); // Box BDD
 var messenger = document.getElementById("messenger"); // Box avec messages
+var online_users = document.getElementById("online_users"); // Utilisateurs en ligne
 
-// Évènements
-msgbox.addEventListener("keypress", writingMsg); // En train d'écrire...
-envoyer.addEventListener("click", envoiMsg); // Envoyer le message
+var signin = document.getElementById("signin"); // Bouton inscription
+var signin_modal = document.getElementById("signin_modal"); // Modal inscription
+var signin_form_button = document.getElementById("signin_validate"); // Bouton formulaire
 
+var login = document.getElementById("login"); // Bouton connexion
+var login_modal = document.getElementById("login_modal"); // Modal connexion
+var login_form_button = document.getElementById("login_validate"); //Bouton formulaire
 
-// Boucles
-setInterval(bddToMessenger, 3000);
+var close_modal = document.getElementsByClassName("close"); // Bouton fermeture modal
 
-// Ajax
-function save_bdd() {
+/// Évènements
+if (msgbox || envoyer) {
+    msgbox.addEventListener("keypress", writingMsg); // En train d'écrire...
+    envoyer.addEventListener("click", envoiMsg); // Envoyer le message
+
+    // Boucles 
+    setInterval(bddToMessenger, 4000);
+    setInterval(online_users_check, 30000);
+
+    // Au chargement de la page
+    online_users_check()
+} 
+
+if (signin || login) {
+    // Évènements
+    signin_form_button.addEventListener("click", signin_user());
+    // login_form_button.addEventListener("click", login_user());
+
+    // Lorsqu'on clique sur le bouton
+    signin.onclick = function() { signin_modal.style.display = "block"; };
+    login.onclick = function() { login_modal.style.display = "block"; };
+
+    // Lorsqu'on clique sur le bouton de fermeture du modal
+    close_modal[0].onclick = function() {signin_modal.style.display = "none";};
+    close_modal[1].onclick = function() {login_modal.style.display = "none";};
+
+    // Lorsque l'on clique en dehors du modal, fermer le modal
+    window.onclick = function(event) {
+    if (event.target == login_modal || event.target == signin_modal) {
+        login_modal.style.display = "none";
+        signin_modal.style.display = "none";
+        }
+    };
+}
+
+// Fonctions de messagerie
+
+function addEmoji(emoji) { msgbox.value += emoji; }
+
+function online_users_check() {
+    $.ajax({ // Mettre à jour le statut de connexion
+        type: "POST",
+        headers: {"Access-Control-Allow-Origin": "*"},
+        url: "http://localhost:5000/api/user/status"
+    }).done(function (data) {
+        if (data != "False") { messenger.innerHTML = data; }
+    });  
+
+    $.ajax({ // Mettre à jour le nombre de personnes connectées
+        type: "GET",
+        headers: {"Access-Control-Allow-Origin": "*"},
+        url: "http://localhost:5000/api/users/online"
+    }).done(function (data) {
+        if (data != "False") { online_users.innerHTML = data; }
+    });  
+}
+
+function signin_user() {
+    var pseudo = document.getElementById("signin_username").value;
+    var pw = document.getElementById("signin_pw").value;
+
+    $.ajax({ // Enregistre l'utilisateur dans la BDD
+        type: "GET",
+        headers: {"Access-Control-Allow-Origin": "*"},
+        url: "http://localhost:5000/api/user/new/"+encodeURI(pseudo)+"/"+encodeURI(pw)
+    }).done(function (data) {
+        if (data == "True") {
+            signin_modal.style.backgroundColor = "none";
+        } else {
+
+        }
+    });
+}
+
+function save_bdd() { // Enregistre à la base de données
     var ajax = new XMLHttpRequest();
     var url = "http://localhost:5000/api/message/new/" + encodeURI(msgbox.value)
     ajax.open("POST", url, true);
@@ -25,52 +101,30 @@ function writingMsg() { // Afficher le en train d'écrire
     console.log("Vous êtes en train d'écrire...");
     bdd.style.backgroundColor = "gray";
     bdd.innerHTML = "Vous êtes en train d'écrire...";
-    if (event.key == "Enter") {
-        envoiMsg();
-    };
+    if (event.key == "Enter") { envoiMsg(); };
   }
 
 function bddToMessenger() {
-        var ajax = new XMLHttpRequest();
-        var url = 'http://localhost:5000/api/messages';
-        ajax.open("GET", url, true);
-        ajax.send();
+    $.ajax({ // Retourrner tous les messages de la BDD
+        type: "GET",
+        headers: {"Access-Control-Allow-Origin": "*"},
+        url: "http://localhost:5000/api/messages"
+    }).done(function (data) {
+        if (data != "False") {
+            messenger.innerHTML = data;
+            var elem = document.getElementById('messenger');
+            elem.scrollTop = elem.scrollHeight;
+        }
+    });
 
-        ajax.onload = function(){
-            if (ajax.status == 200){
-                var reponse = ajax.response;
-                messenger.innerHTML = reponse;
-                var elem = document.getElementById('messenger');
-                elem.scrollTop = elem.scrollHeight;
-            } 
-            else{
-                console.log('Impossible de récupérer la demande Ajax !');
-            }
-          }
-
-        bdd.style.backgroundColor = null;
-        bdd.innerHTML = null;
-    };
-
-function addEmoji(emoji) {
-    msgbox.value += emoji;
-}
+    bdd.style.backgroundColor = null;
+    bdd.innerHTML = null;
+};
 
 function envoiMsg() {
     save_bdd();
     msgbox.value = null;
     bdd.innerHTML = "Traitement...";
     bdd.style.backgroundColor = "orange";
-    
-    
 }
-
-// function bddToMessenger() {
-//     if (bdd.innerHTML) {
-//         console.log("un message!");
-//         // On a un message, traiter et envoyer au messenger
-//         messenger.innerHTML = messenger.innerHTML + bdd.innerHTML + "<br>";
-//         bdd.innerHTML = null;
-//     }
-// }
 
